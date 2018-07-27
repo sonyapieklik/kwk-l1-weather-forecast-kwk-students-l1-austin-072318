0334db11e4a1368eda7fd793e796053d7e61ff8c
sonyapieklik <piesonya@gmail.com>
2018-07-27 15:13:22 +0000

    automatically backed up by learn

--- class.rb
+++ class.rb
@@ -1,7 +1,7 @@
 class PatMcGrath
   
 attr_accessor :shadow1, :shadow2, :shadow3, :name 
-def initalize(shadow1, shadow2, shadow3)
+def initalize (shadow1, shadow2, shadow3)
   @shadow1 = shadow1
   @shadow2 = shadow2
   @shadow3 = shadow3
