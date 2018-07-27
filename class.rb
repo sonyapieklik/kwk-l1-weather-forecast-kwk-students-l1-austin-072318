6366ed9d7dc45e0865abf4c21390b819d3b3982b
sonyapieklik <piesonya@gmail.com>
2018-07-27 15:10:31 +0000

    automatically backed up by learn

--- class.rb
+++ class.rb
@@ -1,11 +1,11 @@
 class PatMcGrath
-  attr_accessor :shadow1, :shadow2, :shadow3, :name 
-  def initalize(shadow1, shadow2, shadow3)
-    @shadow1 = shadow1
-    @shadow2 = shadow2
-    @shadow3 = shadow3
+attr_accessor :shadow1, :shadow2, :shadow3, :name 
+def initalize(shadow1, shadow2, shadow3)
+  @shadow1 = shadow1
+  @shadow2 = shadow2
+  @shadow3 = shadow3
   
-  end
+end
   
   def palette 
     puts "The #{@name} Pat McGrath palette contains shadows such as #{@shadow1}, #{@shadow2}, and #{@shadow3}."
