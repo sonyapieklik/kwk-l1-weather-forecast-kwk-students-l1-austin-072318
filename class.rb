68606efc9df6311e72132163b1a0242bf999f3aa
sonyapieklik <piesonya@gmail.com>
2018-07-27 15:06:20 +0000

    automatically backed up by learn

--- class.rb
+++ class.rb
@@ -4,15 +4,17 @@ class Pat_McGrath
     @shadow1 = shadow1
     @shadow2 = shadow2
     @shadow3 = shadow3
-    @name = name
+  
   end
   
   def palette 
     puts "The #{name} Pat McGrath palette contains shadows such as #{shadow1}, #{shadow2}, and #{shadow3}."
   end
+  
 end
 
-palette1 = Pat_McGrath.new("Substance", "Blitz_Blue", "Depth", "Subliminal")
+palette1 = Pat_McGrath.new("Substance","Blitz_Blue", "Depth")
+palette1.name = "Subliminal"
 
 puts palette1.palette
   
